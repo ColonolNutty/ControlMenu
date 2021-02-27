c6d29b49f5ca8db2446a8e51c36e7e87bca28266
ColonolNutty <ColonolNutty@hotmail.com>
2021-02-27 15:36:46 -0700

    Initial upload

---
+++ Release/Sims4ControlMenu/README.txt
@@ -0,0 +1,11 @@
+
+----------------- Main -----------------
+
+Drag and drop everything directly into your "The Sims 4/Mods" folder
+
+Delete the existing files from previous versions if they exist!
+
+The final paths for the files should be the following:
+
+The Sims 4/Mods/sims4controlmenu.package
+The Sims 4/Mods/sims4controlmenu.ts4script
---
+++ Scripts/compile/compile_scripts.py
@@ -0,0 +1,16 @@
+"""
+The Sims 4 Control Menu is licensed under the Creative Commons Attribution 4.0 International public license (CC BY 4.0).
+https://creativecommons.org/licenses/by/4.0/
+https://creativecommons.org/licenses/by/4.0/legalcode
+
+Copyright (c) COLONOLNUTTY
+"""
+from Utilities.compiler import compile_module
+
+
+compile_module(
+    root='..\\..\\Release\\Sims4ControlMenu',
+    mod_scripts_folder='..',
+    include_folders=('sims4controlmenu',),
+    mod_name='sims4controlmenu'
+)
