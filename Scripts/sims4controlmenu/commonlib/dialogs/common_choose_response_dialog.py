65925ff754ea60b0d19da7342ebc47e677f73bdf
ColonolNutty <ColonolNutty@hotmail.com>
2021-03-01 11:47:07 -0700

    Updates:
    - Added a button option dialog

--- Scripts/sims4controlmenu/commonlib/dialogs/common_choose_response_dialog.py
+++ Scripts/sims4controlmenu/commonlib/dialogs/common_choose_response_dialog.py
@@ -1,11 +1,10 @@
 """
-The Sims 4 Community Library is licensed under the Creative Commons Attribution 4.0 International public license (CC BY 4.0).
+The Sims 4 Control Menu is licensed under the Creative Commons Attribution 4.0 International public license (CC BY 4.0).
 https://creativecommons.org/licenses/by/4.0/
 https://creativecommons.org/licenses/by/4.0/legalcode
 
 Copyright (c) COLONOLNUTTY
 """
-
 import sims4.commands
 from typing import Tuple, Any, Callable, Union, Iterator
 
@@ -27,29 +26,29 @@ from sims4controlmenu.modinfo import ModInfo
 from ui.ui_dialog import UiDialogOption
 
 
-class CommonChooseButtonDialog(CommonDialog):
-    """CommonChooseButtonDialog(\
+class CommonChooseResponseDialog(CommonDialog):
+    """CommonChooseResponseDialog(\
+        mod_identity,\
         title_identifier,\
         description_identifier,\
-        buttons,\
+        responses,\
         title_tokens=(),\
-        description_tokens=(),\
-        mod_identity=None\
+        description_tokens=()\
     )
 
-    Create a dialog that prompts the player to choose a button.
+    Create a dialog that prompts the player to choose a response.
 
-    .. note:: To see an example dialog, run the command :class:`s4clib_testing.show_choose_button_dialog` in the in-game console.
+    .. note:: To see an example dialog, run the command :class:`s4clib_testing.show_choose_response_dialog` in the in-game console.
 
     .. highlight:: python
     .. code-block:: python
 
-        def _common_testing_show_choose_button_dialog():
+        def _common_testing_show_choose_response_dialog():
 
             def _on_chosen(choice: str, outcome: CommonChoiceOutcome):
                 pass
 
-            buttons: Tuple[CommonUiDialogResponse] = (
+            responses: Tuple[CommonUiDialogResponse] = (
                 CommonUiDialogResponse(
                     1,
                     'Value 1',
@@ -65,10 +64,11 @@ class CommonChooseButtonDialog(CommonDialog):
             description_tokens = (CommonLocalizationUtils.create_localized_string(CommonStringId.TESTING_TEST_TEXT_WITH_SIM_FIRST_AND_LAST_NAME, tokens=(CommonSimUtils.get_active_sim_info(),), text_color=CommonLocalizedStringColor.BLUE),)
 
             active_sim_info = CommonSimUtils.get_active_sim_info()
-            dialog = CommonChooseButtonDialog(
+            dialog = CommonChooseResponseDialog(
+                ModInfo.get_identity(),
                 CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
                 CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
-                buttons,
+                responses,
                 title_tokens=title_tokens,
                 description_tokens=description_tokens,
             )
@@ -81,8 +81,8 @@ class CommonChooseButtonDialog(CommonDialog):
     :type title_identifier: Union[int, str, LocalizedString, CommonStringId]
     :param description_identifier: The description to display in the dialog.
     :type description_identifier: Union[int, str, LocalizedString, CommonStringId]
-    :param buttons: The choices that can be chosen.
-    :type buttons: Iterator[CommonUiDialogResponse]
+    :param responses: The choices that can be chosen.
+    :type responses: Iterator[CommonUiDialogResponse]
     :param title_tokens: Tokens to format into the title.
     :type title_tokens: Iterator[Any], optional
     :param description_tokens: Tokens to format into the description.
@@ -93,16 +93,16 @@ class CommonChooseButtonDialog(CommonDialog):
     # noinspection PyMissingOrEmptyDocstring
     @property
     def log_identifier(self) -> str:
-        return 's4cl_choose_object_dialog'
+        return 's4cl_choose_response_dialog'
 
     def __init__(
         self,
+        mod_identity: CommonModIdentity,
         title_identifier: Union[int, str, LocalizedString, CommonStringId],
         description_identifier: Union[int, str, LocalizedString, CommonStringId],
-        buttons: Iterator[CommonUiDialogResponse],
+        responses: Iterator[CommonUiDialogResponse],
         title_tokens: Iterator[Any]=(),
-        description_tokens: Iterator[Any]=(),
-        mod_identity: CommonModIdentity=None
+        description_tokens: Iterator[Any]=()
     ):
         super().__init__(
             title_identifier,
@@ -111,38 +111,36 @@ class CommonChooseButtonDialog(CommonDialog):
             description_tokens=description_tokens,
             mod_identity=mod_identity
         )
-        self._buttons = tuple(buttons)
+        self._responses = tuple(responses)
 
     @property
-    def buttons(self) -> Tuple[CommonUiDialogResponse]:
-        """The buttons to display in the dialog.
+    def responses(self) -> Tuple[CommonUiDialogResponse]:
+        """The responses to display in the dialog."""
+        return self._responses
 
-        :return: A collection of buttons added to the dialog.
-        :rtype: Tuple[CommonUiDialogResponse]
-        """
-        return self._buttons
+    def add_response(self, response: CommonUiDialogResponse, *_, **__):
+        """add_response(response, *_, **__)
 
-    def add_button(self, button: CommonUiDialogResponse, *_, **__):
-        """add_button(button, *_, **__)
+        Add a response to the dialog.
 
-        Add a button to the dialog.
-
-        :param button: The button to add.
-        :type button: CommonUiDialogResponse
+        :param response: The response to add.
+        :type response: CommonUiDialogResponse
         """
         try:
-            self._buttons += (button,)
+            self._responses += (response,)
         except Exception as ex:
-            self.log.error('add_row', exception=ex)
+            self.log.error('add_response', exception=ex)
 
     def show(
         self,
-        on_chosen: Callable[[Any, CommonChoiceOutcome], Any]=CommonFunctionUtils.noop,
+        on_chosen: Callable[[Any, CommonChoiceOutcome], None]=CommonFunctionUtils.noop,
+        on_previous: Callable[[], None]=CommonFunctionUtils.noop,
         dialog_options: UiDialogOption=0,
         sim_info: SimInfo=None
     ):
         """show(\
             on_chosen=CommonFunctionUtils.noop,\
+            on_previous=CommonFunctionUtils.noop,\
             dialog_options=0,\
             sim_info=None,\
         )
@@ -151,6 +149,8 @@ class CommonChooseButtonDialog(CommonDialog):
 
         :param on_chosen: A callback invoked upon the player choosing something from the list. Default is CommonFunctionUtils.noop.
         :type on_chosen: Callable[[Any, CommonChoiceOutcome], optional
+        :param on_previous: A callback performed when the previous response is chosen. Default is no operation.
+        :type on_previous: Callable[[], None], optional
         :param dialog_options: Options to apply to the dialog, such as removing the close button. Default is no options.
         :type dialog_options: UiDialogOption, optional
         :param sim_info: The Sim that will appear in the dialog image. The default Sim is the Active Sim. Default is None.
@@ -159,6 +159,7 @@ class CommonChooseButtonDialog(CommonDialog):
         try:
             return self._show(
                 on_chosen=on_chosen,
+                on_previous=on_previous,
                 dialog_options=dialog_options,
                 sim_info=sim_info
             )
@@ -167,35 +168,58 @@ class CommonChooseButtonDialog(CommonDialog):
 
     def _show(
         self,
-        on_chosen: Callable[[Any, CommonChoiceOutcome], bool]=CommonFunctionUtils.noop,
+        on_chosen: Callable[[Any, CommonChoiceOutcome], None]=CommonFunctionUtils.noop,
+        on_previous: Callable[[], None]=CommonFunctionUtils.noop,
         dialog_options: UiDialogOption=0,
         sim_info: SimInfo=None
     ):
-        def _on_chosen(choice: Any, outcome: CommonChoiceOutcome) -> bool:
+        def _on_chosen(choice: Any, outcome: CommonChoiceOutcome) -> None:
             try:
-                self.log.format_with_message('Choose button choice made.', choice=choice)
-                result = on_chosen(choice, outcome)
-                self.log.format_with_message('Finished handling choose object _show.', result=result)
-                return result
+                self.log.format_with_message('Choice made.', choice=choice)
+                on_chosen(choice, outcome)
+                self.log.debug('Finished handling _show.')
             except Exception as ex:
                 self.log.error('Error occurred on choosing a value.', exception=ex)
-            return False
 
         _dialog = self.build_dialog(
             on_chosen=_on_chosen,
+            on_previous=on_previous,
             dialog_options=dialog_options,
             sim_info=sim_info
         )
+        if _dialog is None:
+            self.log.error('An error occurred when building the dialog!')
+            return
         self.log.debug('Showing dialog.')
         _dialog.show_dialog()
 
-    # noinspection PyMissingOrEmptyDocstring
     def build_dialog(
         self,
-        on_chosen: Callable[[Any, CommonChoiceOutcome], bool]=CommonFunctionUtils.noop,
-        dialog_options: UiDialogOption=0,
+        on_chosen: Callable[[Any, CommonChoiceOutcome], None]=CommonFunctionUtils.noop,
+        on_previous: Callable[[], None]=CommonFunctionUtils.noop,
+        dialog_options: Union[UiDialogOption, int]=0,
         sim_info: SimInfo=None
     ) -> Union[CommonUiResponseDialog, None]:
+        """build_dialog(\
+            on_chosen=CommonFunctionUtils.noop,\
+            on_previous=CommonFunctionUtils.noop,\
+            dialog_options=0,\
+            sim_info=None\
+        )
+
+        Build the dialog.
+
+        :param on_chosen: A callback performed when a choice is made. Default is no operation.
+        :type on_chosen: Callable[[Any, CommonChoiceOutcome], None], optional
+        :param on_previous: A callback performed when the previous response is chosen. Default is no operation.
+        :type on_previous: Callable[[], None], optional
+        :param dialog_options: Display options for the dialog, such as hiding the close button. Default is no display options.
+        :type dialog_options: UiDialogOption, optional
+        :param sim_info: A Sim that will appear in the top left image when the dialog is shown. If set to None, the active Sim will be used. Default is None.
+        :type sim_info: SimInfo, optional
+        :return: The built dialog or None if a problem occurs.
+        :rtype: Union[CommonUiResponseDialog, None]
+        """
         self.log.format_with_message('Attempting to build dialog.', dialog_options=dialog_options)
 
         _dialog = self._create_dialog(
@@ -209,42 +233,39 @@ class CommonChooseButtonDialog(CommonDialog):
         if on_chosen is None:
             raise ValueError('on_chosen was None.')
 
-        if len(self.buttons) == 0:
-            raise AssertionError('No rows have been provided. Add rows to the dialog before attempting to display it.')
+        if len(self.responses) == 0:
+            raise AssertionError('No responses have been provided. Add responses to the dialog before attempting to display it.')
 
-        def _on_chosen(dialog: CommonUiResponseDialog) -> bool:
+        def _on_chosen(dialog: CommonUiResponseDialog) -> None:
             try:
-                self.log.debug('Choice made.')
                 if dialog.cancelled:
                     self.log.debug('Dialog cancelled.')
-                    return on_chosen(None, CommonChoiceOutcome.CANCEL)
+                    on_chosen(None, CommonChoiceOutcome.CANCEL)
+                    return
                 if dialog.previous:
                     self.log.debug('Dialog previous.')
-                    # noinspection PyTypeChecker
-                    return on_chosen(None, 4)
-                self.log.debug('Choice not made.')
+                    on_previous()
+                    return
                 choice = dialog.get_response_value()
                 self.log.format_with_message('Choice made.', choice=choice)
-                result = on_chosen(choice, CommonChoiceOutcome.CHOICE_MADE)
-                self.log.format_with_message('Finished handling choose object _on_chosen.', result=result)
-                return result
+                on_chosen(choice, CommonChoiceOutcome.CHOICE_MADE)
+                self.log.debug('Finished handling _on_chosen.')
             except Exception as ex:
                 self.log.error('Error occurred on choosing a value.', exception=ex)
-            return False
 
-        self.log.debug('Adding always visible rows.')
-        self._setup_dialog_rows(_dialog)
+        self._setup_dialog(_dialog)
 
         self.log.debug('Adding listener.')
         _dialog.add_listener(_on_chosen)
         return _dialog
 
-    def _setup_dialog_rows(
+    def _setup_dialog(
         self,
         _dialog: CommonUiResponseDialog
     ):
-        for button in self.buttons:
-            _dialog.add_response(button)
+        self.log.debug('Setting up dialog.')
+        for response in self.responses:
+            _dialog.add_response(response)
 
     def _create_dialog(
         self,
@@ -252,10 +273,11 @@ class CommonChooseButtonDialog(CommonDialog):
         sim_info: SimInfo=None
     ) -> Union[CommonUiResponseDialog, None]:
         try:
-            self.log.debug('Building dialog without categories.')
+            self.log.debug('Creating dialog.')
             return CommonUiResponseDialog.TunableFactory().default(
                 sim_info or CommonSimUtils.get_active_sim_info(),
                 tuple(),
+                dialog_options=dialog_options,
                 text=lambda *_, **__: self.description,
                 title=lambda *_, **__: self.title
             )
@@ -264,16 +286,16 @@ class CommonChooseButtonDialog(CommonDialog):
         return None
 
 
-@sims4.commands.Command('s4clib_testing.show_choose_button_dialog', command_type=sims4.commands.CommandType.Live)
-def _common_testing_show_choose_button_dialog(_connection: int=None):
+@sims4.commands.Command('s4clib_testing.show_choose_response_dialog', command_type=sims4.commands.CommandType.Live)
+def _common_testing_show_choose_response_dialog(_connection: int=None):
     output = sims4.commands.CheatOutput(_connection)
-    output('Showing test choose button dialog.')
+    output('Showing test choose response dialog.')
 
     def _on_chosen(choice: str, outcome: CommonChoiceOutcome):
         output('Chose {} with result: {}.'.format(pformat(choice), pformat(outcome)))
 
     try:
-        buttons: Tuple[CommonUiDialogResponse] = (
+        responses: Tuple[CommonUiDialogResponse] = (
             CommonUiDialogResponse(
                 1,
                 'Value 1',
@@ -289,10 +311,11 @@ def _common_testing_show_choose_button_dialog(_connection: int=None):
         description_tokens = (CommonLocalizationUtils.create_localized_string(CommonStringId.TESTING_TEST_TEXT_WITH_SIM_FIRST_AND_LAST_NAME, tokens=(CommonSimUtils.get_active_sim_info(),), text_color=CommonLocalizedStringColor.BLUE),)
 
         active_sim_info = CommonSimUtils.get_active_sim_info()
-        dialog = CommonChooseButtonDialog(
+        dialog = CommonChooseResponseDialog(
+            ModInfo.get_identity(),
             CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
             CommonStringId.TESTING_TEST_TEXT_WITH_STRING_TOKEN,
-            buttons,
+            responses,
             title_tokens=title_tokens,
             description_tokens=description_tokens,
         )
