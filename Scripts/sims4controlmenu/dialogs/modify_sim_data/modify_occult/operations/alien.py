df36dbf8f2d9f4ba5597d81c6eb36a833c08f78b
ColonolNutty <ColonolNutty@hotmail.com>
2021-03-02 13:18:41 -0700

    Updates:
    - When adding the alien occult type to a Sim, their appearance, such as
    skintone, should not change.

--- Scripts/sims4controlmenu/dialogs/modify_sim_data/modify_occult/operations/alien.py
+++ Scripts/sims4controlmenu/dialogs/modify_sim_data/modify_occult/operations/alien.py
@@ -7,7 +7,10 @@ Copyright (c) COLONOLNUTTY
 """
 from typing import Tuple
 
+from sims.occult.occult_enums import OccultType
 from sims.sim_info import SimInfo
+from sims4communitylib.enums.buffs_enum import CommonBuffId
+from sims4communitylib.utils.sims.common_buff_utils import CommonBuffUtils
 from sims4communitylib.utils.sims.common_occult_utils import CommonOccultUtils
 from sims4controlmenu.commonlib.utils.common_sim_loot_utils import CommonSimLootActionUtils
 from sims4controlmenu.dialogs.modify_sim_data.sim_operation import S4CMSingleSimOperation
@@ -26,7 +29,40 @@ class S4CMAlienAddOp(S4CMSingleSimOperation):
             # loot_Occult_AlienSwitch
             103254
         )
-        return CommonSimLootActionUtils.apply_loot_actions_to_sim(loot_action_ids, sim_info)
+        physique = sim_info.physique
+        facial_attributes = sim_info.facial_attributes
+        voice_pitch = sim_info.voice_pitch
+        voice_actor = sim_info.voice_actor
+        voice_effect = sim_info.voice_effect
+        skin_tone = sim_info.skin_tone
+        flags = sim_info.flags
+        pelt_layers = None
+        if hasattr(sim_info, 'pelt_layers'):
+            pelt_layers = sim_info.pelt_layers
+        base_trait_ids = None
+        if hasattr(sim_info, 'base_trait_ids'):
+            base_trait_ids = list(sim_info.base_trait_ids)
+        genetic_data_b = sim_info.genetic_data
+        if hasattr(genetic_data_b, 'SerializeToString'):
+            genetic_data_b = genetic_data_b.SerializeToString()
+        result = CommonSimLootActionUtils.apply_loot_actions_to_sim(loot_action_ids, sim_info)
+        human_sim_info = sim_info.occult_tracker.get_occult_sim_info(OccultType.HUMAN)
+        human_sim_info.physique = physique
+        human_sim_info.facial_attributes = facial_attributes
+        human_sim_info.voice_pitch = voice_pitch
+        human_sim_info.voice_actor = voice_actor
+        human_sim_info.voice_effect = voice_effect
+        human_sim_info.skin_tone = skin_tone
+        human_sim_info.flags = flags
+        if pelt_layers is not None:
+            human_sim_info.pelt_layers = pelt_layers
+        if base_trait_ids is not None:
+            human_sim_info.base_trait_ids = list(base_trait_ids)
+        if hasattr(human_sim_info.genetic_data, 'MergeFromString'):
+            human_sim_info.genetic_data.MergeFromString(genetic_data_b)
+        else:
+            human_sim_info.genetic_data = genetic_data_b
+        return result
 
 
 class S4CMAlienRemoveOp(S4CMSingleSimOperation):
@@ -36,11 +72,5 @@ class S4CMAlienRemoveOp(S4CMSingleSimOperation):
     def run(self, sim_info: SimInfo) -> bool:
         if not CommonOccultUtils.is_alien(sim_info):
             return False
-        loot_action_ids: Tuple[int] = (
-            # loot_Occult_AlienSwitchOff
-            103255,
-            # loot_Occult_AlienRemove
-            103257
-        )
-        return CommonSimLootActionUtils.apply_loot_actions_to_sim(loot_action_ids, sim_info)
-
+        sim_info.occult_tracker.remove_occult_type(OccultType.ALIEN)
+        return True
