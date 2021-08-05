b226afec95b5fa70b30ef324c03d7c0e7617bab8
ColonolNutty <ColonolNutty@hotmail.com>
2021-08-05 15:27:43 -0600

    Updates:
    - Added an interaction to Summon or Teleport Sims from the relationship
    panel to your Sim.

---
+++ Scripts/sims4controlmenu/interactions/sim_spawn/summon_sim_from_rel_panel.py
@@ -0,0 +1,72 @@
+"""
+The Sims 4 Control Menu is licensed under the Creative Commons Attribution 4.0 International public license (CC BY 4.0).
+https://creativecommons.org/licenses/by/4.0/
+https://creativecommons.org/licenses/by/4.0/legalcode
+
+Copyright (c) COLONOLNUTTY
+"""
+from typing import Any
+from event_testing.results import TestResult
+from interactions import ParticipantType
+from interactions.context import InteractionContext
+from sims.sim import Sim
+from sims4communitylib.classes.interactions.common_immediate_super_interaction import CommonImmediateSuperInteraction
+from sims4communitylib.mod_support.mod_identity import CommonModIdentity
+from sims4communitylib.utils.objects.common_object_location_utils import CommonObjectLocationUtils
+from sims4communitylib.utils.sims.common_age_utils import CommonAgeUtils
+from sims4communitylib.utils.sims.common_sim_location_utils import CommonSimLocationUtils
+from sims4communitylib.utils.sims.common_sim_spawn_utils import CommonSimSpawnUtils
+from sims4communitylib.utils.sims.common_sim_utils import CommonSimUtils
+from sims4controlmenu.modinfo import ModInfo
+
+
+class S4CMSummonSimFromRelPanelInteraction(CommonImmediateSuperInteraction):
+    """S4CMSummonSimFromRelPanelInteraction(*_, **__)
+
+    Open a dialog and Summon the target Sim from the Relationship Panel to the Active Sim
+    """
+
+    # noinspection PyMissingOrEmptyDocstring
+    @classmethod
+    def get_mod_identity(cls) -> CommonModIdentity:
+        return ModInfo.get_identity()
+
+    # noinspection PyMissingOrEmptyDocstring
+    @classmethod
+    def get_log_identifier(cls) -> str:
+        return 's4cm_spawn_sim_from_rel_panel'
+
+    # noinspection PyMissingOrEmptyDocstring
+    @classmethod
+    def on_test(cls, interaction_sim: Sim, interaction_target: Any, interaction_context: InteractionContext, **kwargs) -> TestResult:
+        if 'picked_item_ids' not in kwargs:
+            cls.get_log().format_with_message('Failed, No picked targets.', kwargles=kwargs)
+            return TestResult.NONE
+        location = CommonObjectLocationUtils.get_location(interaction_sim)
+        if location is None:
+            cls.get_log().format_with_message('Failed, No Location for Sim.')
+            return TestResult.NONE
+        cls.get_log().format_with_message('Success, can spawn sim from rel panel.', sim_ids=kwargs['picked_item_ids'])
+        return TestResult.TRUE
+
+    # noinspection PyMissingOrEmptyDocstring
+    def on_started(self, interaction_sim: Sim, interaction_target: Any) -> bool:
+        target_location = CommonObjectLocationUtils.get_location(interaction_sim)
+
+        # noinspection PyUnresolvedReferences
+        picked_sim_info = CommonSimUtils.get_sim_info(self.get_participant(ParticipantType.PickedSim))
+
+        if picked_sim_info is None:
+            return False
+        if CommonSimUtils.get_sim_instance(picked_sim_info) is not None:
+            if CommonAgeUtils.is_baby(picked_sim_info):
+                # Sim is spawned, so let's move them to the Active Sim.
+                CommonSimLocationUtils.set_location(picked_sim_info, target_location)
+        else:
+            # Sim is not spawned, so let's spawn them.
+            if CommonAgeUtils.is_baby(picked_sim_info):
+                from sims.baby.baby_utils import create_and_place_baby
+                create_and_place_baby(picked_sim_info, position=target_location.transform.translation, routing_surface=target_location.routing_surface)
+            else:
+                CommonSimSpawnUtils.spawn_sim(picked_sim_info, location=target_location)
+        return True
