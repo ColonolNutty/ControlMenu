9a89dbdf17734654f32577ea22dd7e23210aec6d
ColonolNutty <ColonolNutty@hotmail.com>
2022-08-25 16:00:50 -0600

    Updates:
    - Fixed an issue with the induce labor operation

--- Scripts/controlmenu/dialogs/modify_sim_data/pregnancy/operations/induce_labor.py
+++ Scripts/controlmenu/dialogs/modify_sim_data/pregnancy/operations/induce_labor.py
@@ -10,10 +10,8 @@ from typing import Callable
 from sims.sim_info import SimInfo
 from sims4communitylib.classes.testing.common_execution_result import CommonExecutionResult
 from sims4communitylib.utils.common_function_utils import CommonFunctionUtils
-from sims4communitylib.utils.sims.common_buff_utils import CommonBuffUtils
 from sims4communitylib.utils.sims.common_sim_pregnancy_utils import CommonSimPregnancyUtils
 from controlmenu.dialogs.modify_sim_data.single_sim_operation import CMSingleSimOperation
-from controlmenu.enums.string_identifiers import CMStringId
 
 
 class CMInduceLaborOp(CMSingleSimOperation):
@@ -29,10 +27,9 @@ class CMInduceLaborOp(CMSingleSimOperation):
         if not CommonSimPregnancyUtils.is_pregnant(sim_info):
             on_completed(CommonExecutionResult(False, reason=f'{sim_info} is not pregnant.'))
             return False
-        buff_id = CommonSimPregnancyUtils.get_in_labor_buff(sim_info)
-        if buff_id == -1:
-            on_completed(CommonExecutionResult(False, reason=f'No in labor buff found for {sim_info}'))
-            return False
-        result = CommonBuffUtils.add_buff(sim_info, buff_id, buff_reason=CMStringId.DEBUG_COMMAND)
-        on_completed(result)
-        return result.result
+        result = CommonSimPregnancyUtils.induce_labor_in_sim(sim_info)
+        if result:
+            on_completed(CommonExecutionResult.TRUE)
+        else:
+            on_completed(CommonExecutionResult(False, reason=f'Failed to induce labor in {sim_info}'))
+        return result
